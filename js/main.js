7af1448069e20493504eec1a2e0d5c98ad2c9de5
elvece <lvcifferello@gmail.com>
2015-08-02 14:21:45 -0600

    fixed value amounts in html, added javascript calculate and write answer functions, added event listener to button

--- js/main.js
+++ js/main.js
@@ -3,8 +3,30 @@
 //   console.log('sanity check!');
 // });
 
-//global variables
-
 //main javaScript functions
+function calculate(){
+  debugger;
+  var enteredAmount = document.getElementById('amount').value;
+  var selected = document.getElementsByTagName('select')[0].selectedOptions;
+  var num = parseInt(enteredAmount);
+  var percent = Number(selected[0].value);
+  var total = num * percent;
+  return total;
+}
+
+//DOM functions
+
+//changes the inner html of the result div to the tip amount
+function writeAnswer(tipAmount){
+  var answer = document.getElementById('result');
+      result.innerHTML = "$" + tipAmount.toFixed(2);
+}
+
+
+document.getElementById("calculate").addEventListener("click", function(){
+  var result = calculate();
+  writeAnswer(result);
+});
+
 
 //calling functions
