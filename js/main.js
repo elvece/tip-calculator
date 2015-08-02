91a7876d3c31d700b4afa7fa75ad4a8f3cfd38a3
elvece <lvcifferello@gmail.com>
2015-08-02 13:44:06 -0600

    set up js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+//jQuery ready section to refactor in later
+// $(document).on('ready', function() {
+//   console.log('sanity check!');
+// });
+
+//global variables
+
+//main javaScript functions
+
+//calling functions
