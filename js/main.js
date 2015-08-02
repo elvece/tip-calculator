3faae6658adcb6cabc2dd114ef15a6ec82d698e0
elvece <lvcifferello@gmail.com>
2015-08-02 15:39:18 -0600

    changed where result is turned into a number so only using once

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@
 
 //adds button click functionality to run the calculate and writeAnswer functions after getting the value from the text input field and the dropdown menu
 document.getElementById("calculate").addEventListener("click", function(){
-  var enteredAmount = Number(document.getElementById('amount').value.replace("$", ""));
-  var selected = Number(document.getElementsByTagName('select')[0].selectedOptions[0].value);
+
+  var enteredAmount = document.getElementById('amount').value.replace("$", "");
+
+  var selected = document.getElementsByTagName('select')[0].selectedOptions[0].value;
+
   var answer = document.getElementById('result');
-  result.innerHTML = "You should tip $" + calculate(enteredAmount, selected).toFixed(2);
+
+  result.innerHTML = "You should tip $" + Number(calculate(enteredAmount, selected)).toFixed(2);
 });
 
 //jQuery ready section to refactor in later
